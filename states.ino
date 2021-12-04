5147ffffd12587870b320b33f72d7653ab128205
Kaylie Capurro <capurro.kaylie@gmail.com>
2021-12-03 16:20:33 -0800

    states file

---
+++ states.ino
@@ -0,0 +1,11 @@
+//Authors: Kaylie Capurro, Kyle Johnson, Mason Smith
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
